5013f399603a0e374f8fb1be90628c68e4682d23
Kunal1358 <kunalkohli8054@gmail.com>
2021-03-27 11:24:26 +0530

    Part B: Creating list of blogs 3-> Created bloglist.js & Part B Completed

---
+++ scripts/bloglist.js
@@ -0,0 +1,17 @@
+// Funcion to remove Blogs.
+function removeCard(card) {
+    let delCard = document.getElementById(card);
+
+    delCard.parentNode.removeChild(delCard);
+}
+
+function openPost(author, heading, content) {
+    var url =
+            '../html/post.html?heading=' +
+            encodeURIComponent(heading.innerText) +
+            '&author=' +
+            encodeURIComponent(author.innerText) +
+            '&content=' +
+            encodeURIComponent(content.innerText);
+    window.location.href = url;
+}
